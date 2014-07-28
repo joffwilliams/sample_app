c5b6cb6fa93e15f560b643a08d0ee37d82c57081
Joff Williams <joff@williams-family.us>
2014-07-28 22:03:28 +0000

    Removed database.yml

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -31,4 +31,14 @@ describe "Static Pages" do
       expect(page).to have_title('About')
     end
   end
+  describe "Contact page" do
+    it "should have the content 'Contact Us'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact Us')
+    end
+    it "should have the title 'Contact Us'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title('Contact')
+    end
+  end
 end
